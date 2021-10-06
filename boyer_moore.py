b5804ebed7df19ddc214dae5b6c4513cbde2ae77
SBShuvo <sbshuvo.bme.buet@gmail.com>
2021-10-06 12:04:05 +0600

    adding boyer moore

---
+++ boyer_moore.py
@@ -0,0 +1,15 @@
+# It is the algorithm to find a specific pattern on a given text 
+def boyer_moore(text,pattern):
+    print(f"Let's find {pattern} in {text}.......")
+    utext=text.upper()
+    upattern=pattern.upper()
+    bad_character()
+    good_suffix()
+def bad_character():
+    print("here the bad character table will be generated")
+    pass
+def good_suffix():
+    print("here the good suffix table will be generated")
+    pass
+
+boyer_moore()

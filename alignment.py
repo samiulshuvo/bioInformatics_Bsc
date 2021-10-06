c1139d9ce6922d919cfbb83764267446f10849ab
SBShuvo <sbshuvo.bme.buet@gmail.com>
2021-10-06 15:36:04 +0600

    alignment

---
+++ alignment.py
@@ -0,0 +1,13 @@
+# this code is for developing both the global alignment and local alignment algorithm 
+from typing import Pattern
+
+
+class alignment_algo():
+    def __init__(self,seq1,seq2,selection='b'):
+        pass
+    def global_alignment():
+        print("Printing the gl0bal alignment")
+    def local_alignment():
+        print("Printing the local alignment")
+    def printing_the_Pattern():
+        pass
